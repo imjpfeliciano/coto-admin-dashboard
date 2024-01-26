6d05a7e4c6ac5e867111fdbac299af537929b16f
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2024-01-25 21:41:29 -0500

    refactor: base api url

---
+++ app/constants/index.ts
@@ -0,0 +1,2 @@
+export const BASE_API_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || 'https://localhost:3000';
--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,39 +1,36 @@
-import Link from 'next/link'
-import Card from '../../../../components/Card'
-import MaterialIcon from '../../../../components/MaterialIcon'
-import UserForm from '../../../../components/Users/Form'
+import Link from 'next/link';
+import Card from '../../../../components/Card';
+import MaterialIcon from '../../../../components/MaterialIcon';
+import UserForm from '../../../../components/Users/Form';
+import { BASE_API_URL } from '../../../constants';
 
 const fetchUserDetails = async (id: string) => {
-  const res = await fetch(`https://localhost:3000/v1/api/users/${id}`)
-  const { user } = await res.json()
-  
-  return user
-}
+  const res = await fetch(`${BASE_API_URL}/v1/api/users/${id}`);
+  const { user } = await res.json();
+
+  return user;
+};
 
 interface ProfilePageProps {
-  params: any
+  params: any;
 }
 
 const ProfilePage = async ({ params }: ProfilePageProps) => {
-  const { id } = params
+  const { id } = params;
 
-  const user = await fetchUserDetails(id)
+  const user = await fetchUserDetails(id);
 
   return (
-      <Card>
-        <div className='pb-2 mb-2 border-b-2 flex flex-row justify-between align-center'>
-          <h1 className='text-xl font-bold'>Datos del Usuario</h1>
-          <Link href='/users'>
-            <MaterialIcon iconName='close' className='text-gray-500' />
-          </Link>
-        </div>
-        <UserForm
-          newUser={false}
-          userId={id}
-          user={user}
-        />
-      </Card>
-  )
-}
+    <Card>
+      <div className='pb-2 mb-2 border-b-2 flex flex-row justify-between align-center'>
+        <h1 className='text-xl font-bold'>Datos del Usuario</h1>
+        <Link href='/users'>
+          <MaterialIcon iconName='close' className='text-gray-500' />
+        </Link>
+      </div>
+      <UserForm newUser={false} userId={id} user={user} />
+    </Card>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
